34ff21f0f576cb65c4ffb22c4205e9cbabc83a45
Lucien <1155064923@link.cuhk.edu.hk>
2020-04-07 04:21:03 +0800

    smaller network so the training is faster now

--- this_is_my_agent.py
+++ this_is_my_agent.py
@@ -38,8 +38,8 @@ def student_compute_action_function(num_envs=1):
     Run this file directly to make sure everything is fine.
     """
     # [TODO] rewrite this function
-    my_agent_log_dir = "data/competitive/PPO"
-    my_agent_suffix = "iter2000"
+    my_agent_log_dir = "data/vgg/PPO"
+    my_agent_suffix = "iter600"
 
     checkpoint_path = osp.join(my_agent_log_dir,
                                "checkpoint-{}.pkl".format(my_agent_suffix))
